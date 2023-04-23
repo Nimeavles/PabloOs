3931b486f0c62acad8502f684550202d3df2db3e
Nimeavles <prlf505@gmail.com>
2023-04-23 21:22:25 +0200

    feat: 02-basic-bootloader-with-hello-text

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,6 +1,16 @@
-loop:
-    jmp loop
+mov ah, 0x0e
+mov al, "H"
+int 0x10
+mov al, "e"
+int 0x10
+mov al, "l"
+int 0x10
+int 0x10
+mov al, "o"
+int 0x10
 
-times 510-($-$$) db 0 ;fill with 510 zeros minus the size of the previus code
+jmp $
 
-dw 0xaa55; Magic number
+times 510-($-$$) db 0
+
+dw 0xaa55
