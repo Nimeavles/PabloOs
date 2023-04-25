5e6497b9a1de20a588775d3282d22779a9bf816a
Nimeavles <prlf505@gmail.com>
2023-04-25 18:58:47 +0200

    feat: 03-boot-sector-programming

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,16 +1,28 @@
+[org 0x7c00]
+
 mov ah, 0x0e
-mov al, "H"
+;First attempt
+mov al, the_secret
 int 0x10
-mov al, "e"
+
+;Second attempt
+mov al, [the_secret]
 int 0x10
-mov al, "l"
+
+;Third attempt
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
-int 0x10
-mov al, "o"
+
+;Fourth attempt
+mov al, [0x7c1e]
 int 0x10
 
 jmp $
 
-times 510-($-$$) db 0
+the_secret:
+    db "X"
 
+times 510-($-$$) db 0
 dw 0xaa55
